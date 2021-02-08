bb9672849dd5f1707508ed7c64339f8d5f1a35d2
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-02-08 12:34:08 -0800

    ahora el formulario guarda y muestra errores de validacion

--- Back-end/Ruby/Rails/GoRails/rails_for_beginners/scheduled_tweets/app/controllers/registrations_controller.rb
+++ Back-end/Ruby/Rails/GoRails/rails_for_beginners/scheduled_tweets/app/controllers/registrations_controller.rb
@@ -6,4 +6,21 @@ class RegistrationsController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: "successfully created account"
+    else
+      # flash[:alert] = "Something was wrong"
+      render :new
+    end
+  end
+
+
+  private
+  def user_params
+    params.required(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
