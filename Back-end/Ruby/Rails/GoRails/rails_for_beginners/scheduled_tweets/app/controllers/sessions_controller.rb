d8f60a33a87adb3393a43e0a36ef246180feff57
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-02-09 12:25:00 -0800

    Generado session destroy o Logout

---
+++ Back-end/Ruby/Rails/GoRails/rails_for_beginners/scheduled_tweets/app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out"
+  end
+
+end
