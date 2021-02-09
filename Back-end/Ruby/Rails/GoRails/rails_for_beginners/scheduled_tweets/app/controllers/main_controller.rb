a1cecc899fb3469019c6acf9af3aaa747ab36e99
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-02-09 12:14:17 -0800

    Al crear el usuario se genera una session Cookie y muestra email

--- Back-end/Ruby/Rails/GoRails/rails_for_beginners/scheduled_tweets/app/controllers/main_controller.rb
+++ Back-end/Ruby/Rails/GoRails/rails_for_beginners/scheduled_tweets/app/controllers/main_controller.rb
@@ -3,8 +3,9 @@
 class MainController < ApplicationController
 
   def index
-    flash.now[:notice] = "Logged in successfully"
-    flash.now[:alert] = "invalid email or password"
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 
 end
