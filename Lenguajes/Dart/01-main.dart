1eddc4ca2a3923a65cd2ab09b63577ea0f646f90
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-02-10 13:14:26 -0800

    Mi primer hola mundo con Dart

---
+++ Lenguajes/Dart/01-main.dart
@@ -0,0 +1,15 @@
+//Cuando no se retorna nada se usa void
+void main() {
+//Comentario en una sola linea
+
+/*
+  Comentario 
+  en 
+  multi linea
+*/
+
+  String nombre = "Obelich";
+
+  //Para hacer interpolación se usa el simbolo de dolar $
+  print("Hola $nombre");
+}
