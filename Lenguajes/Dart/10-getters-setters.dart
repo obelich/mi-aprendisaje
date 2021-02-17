25a55377f08fe01154452789b39973a9fea42bdd
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-02-16 17:23:11 -0800

    Iniciando a trabajar con Getters y Setters Dart

---
+++ Lenguajes/Dart/10-getters-setters.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+void main() {
+
+  final cuadrado = new Cuadrado();
+}
+
+class Cuadrado {
+
+  double _lado; // Si las propiedades tienen un guion bajo se hacen privadas _lado
+  double _area;
+
+
+}
