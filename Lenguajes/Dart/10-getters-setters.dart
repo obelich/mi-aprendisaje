55ca4b39c47acb1d9f119a95372cffaba253f155
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-02-17 12:02:14 -0800

    usando funciones con Getters y Setters

--- Lenguajes/Dart/10-getters-setters.dart
+++ Lenguajes/Dart/10-getters-setters.dart
@@ -3,12 +3,35 @@ import 'dart:convert';
 void main() {
 
   final cuadrado = new Cuadrado();
+
+  cuadrado.lado = 10;
+  print(cuadrado);
+
+  print('área: ${ cuadrado.area}');
 }
 
 class Cuadrado {
 
   double _lado; // Si las propiedades tienen un guion bajo se hacen privadas _lado
-  double _area;
+  // double _area;
+
+  set lado(double valor) {
+    if ( valor <= 0) {
+      throw('El lado no puede ser menor o igual a 0');
+    }
+
+    _lado = valor;
+  }
+
+  //funcion normal de getter
+  // double get area {
+  //   return _lado * _lado;
+  // }
+
+  //funcion de flecha
+  double get area => _lado * _lado;
+
+  toString() => 'Lado: $_lado';
 
 
 }
