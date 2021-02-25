3b7a4ab538b515bbfdd742a7f92941531e713759
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-02-24 22:15:09 -0800

    Haciendo que los items de la lista se generen con un ciclo for

--- Mobile/Flutter/Udemy/flutter-ios-android-fernando-herrera/componentes/lib/src/pages/home_temp.dart
+++ Mobile/Flutter/Udemy/flutter-ios-android-fernando-herrera/componentes/lib/src/pages/home_temp.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Componentes Temp"),
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Hola Obelich'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-        ],
-      ),
+      body: ListView(children: _crearItems()),
     );
   }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+
+  }
+
+
+
+
+// Final
+
 }
