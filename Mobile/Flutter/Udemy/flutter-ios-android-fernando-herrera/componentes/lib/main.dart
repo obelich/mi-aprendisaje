6a39b259f90ca2c74e138b973f479bb0c45408bb
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-03-14 15:51:35 -0700

    Generando el homage page, o el maint component

--- Mobile/Flutter/Udemy/flutter-ios-android-fernando-herrera/componentes/lib/main.dart
+++ Mobile/Flutter/Udemy/flutter-ios-android-fernando-herrera/componentes/lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:componentes/src/pages/home_temp.dart';
-
+import 'package:componentes/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
-    );
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        home: HomePage());
   }
 }
