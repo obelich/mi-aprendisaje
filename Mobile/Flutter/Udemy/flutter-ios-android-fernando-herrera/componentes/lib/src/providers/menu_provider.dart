d47a67d5fc64c3f0a59dc9c792e86819cf7a1431
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2021-04-03 10:30:26 -0700

    Se genero un metodo future para la carga del menu

--- Mobile/Flutter/Udemy/flutter-ios-android-fernando-herrera/componentes/lib/src/providers/menu_provider.dart
+++ Mobile/Flutter/Udemy/flutter-ios-android-fernando-herrera/componentes/lib/src/providers/menu_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 
 class _MenuProvider {
@@ -7,11 +8,15 @@ class _MenuProvider {
     cargarData();
   }
 
-  cargarData() {
-    rootBundle.loadString('data/menu_opts.json')
-      .then((data) {
-        print(data);
-      });
+  Future<List<dynamic>> cargarData() async {
+
+
+    final resp = await rootBundle.loadString('data/menu_opts.json')
+      
+      Map dataMap = json.decode(resp);
+        opciones = dataMap['rutas'];
+
+        return opciones;
   }
 
 }
