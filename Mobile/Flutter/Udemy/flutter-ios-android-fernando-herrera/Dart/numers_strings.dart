f12083a0e33d208200fe9fe0d4223b2b14ac4fee
Oscar Miguel Amezcua Estrella <obelich@protonmail.com>
2022-04-09 12:10:59 -0700

    tipo de dato string y numerico en dart

---
+++ Mobile/Flutter/Udemy/flutter-ios-android-fernando-herrera/Dart/numers_strings.dart
@@ -0,0 +1,18 @@
+void main() {
+  final String nombre = 'Tony';
+  final apellido = 'Stark';
+
+  // Si se declara que es final no se puede modificar
+  // nombre = 'Petter';
+
+  print(nombre);
+
+  print('$nombre $apellido');
+
+  // Números
+  int empleados = 10;
+  double salario = 1856.25;
+
+  print(empleados);
+  print(salario);
+}
